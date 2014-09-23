5143d398637bac55bbfb50fbb22023a616b408dc
Shaun Ford <shaun.deandre@yahoo.com>
2014-09-23 00:11:04 -0400

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PocketChange::Application.config.secret_token = '782c3357dc52d0e78acc03d92f7e7e8a9fa558a0c3e2d8922076e3dc40823fb926e3400c3d5591fb45457874dea5cf0d461c0b3a194c349a7c4623e2a4cf2d78'
